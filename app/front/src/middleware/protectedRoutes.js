83b863202d6b1585a38c0d8b802e1595d0de221e
JocelynTereucan <38298509+JocelynTereucan@users.noreply.github.com>
2025-05-22 13:15:26 -0400

    actualización rutas protegidas

--- app/front/src/middleware/protectedRoutes.js
+++ app/front/src/middleware/protectedRoutes.js
@@ -1,17 +1,50 @@
 import { NextResponse } from "next/server";
 
+function parseJwt(token) {
+  try {
+    return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+  } 
+  catch (e) {
+      return null;
+  }
+}
+
 export function middleware(req) {
+  const { pathname } = req.nextUrl;
   const token = req.cookies.get("token"); // Obtén el token de las cookies
 
+  const protectedPaths = ["/catalog", "/admin", "/mapa", "/products", "/companies"];
+  const isProtected = protectedPaths.some((path) => pathname.startsWith(path));
+
   // Si no hay token y el usuario intenta acceder a una ruta protegida
-  if (!token && req.nextUrl.pathname.startsWith("/catalog")) {
+  if (!token && isProtected) {
     return NextResponse.redirect(new URL("/login", req.url)); // Redirige al login
   }
 
-  return NextResponse.next(); // Permite el acceso si hay un token
+  if (token) {
+    const payload = parseJwt(token.value);
+    const role = payload?.role;
+
+    // 🔒 Rutas por rol
+    const rolePermissions = {
+      superadmin: ["/admin", "/products", "/companies", "/mapa"],
+      admin: ["/products", "/companies", "/mapa"],
+      seller: ["/catalog", "/mapa"]
+    };
+
+    const allowedPaths = rolePermissions[role] || [];
+
+    const isAllowed = allowedPaths.some((path) => pathname.startsWith(path));
+
+    if (!isAllowed && isProtected) {
+      return NextResponse.redirect(new URL("/unauthorized", req.url));
+}
+ }
+
+  return NextResponse.next();
 }
 
 // Configura las rutas protegidas
 export const config = {
-  matcher: ["/admin/:path*"], // Aplica el middleware a las rutas protegidas
+  matcher: ["/path*"], // Aplica el middleware a las rutas protegidas
 };
