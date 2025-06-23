d5b08903bc8f2410d6aa1dd036da7b4641b69d3a
itriscan <cristian.quilodran@alumnos.uach.cl>
2025-06-22 22:32:19 -0400

    Fix error return

--- coordinator/backend/controllers/authController.js
+++ coordinator/backend/controllers/authController.js
@@ -3,25 +3,20 @@ import bcrypt from 'bcryptjs';
 
 
 export async function loginUser(dataUser) {
-    try {
-        const { email, password } = dataUser;
-        // Find user by username
-        const user = await User.findOne({ email });
-        if (!user) {
-            throw new Error('User not found');
-        }
-        // Check password
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            throw new Error('Invalid password');
-        }
-        // Return user data without password
-        const { password: _, ...userData } = user.toObject();
-        return userData;
+   
+    const { email, password } = dataUser;
+    // Find user by username
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new Error('User not found');
     }
-    catch (error) {
-        console.error('Login error:', error);
-        throw new Error('Login failed');
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        throw new Error('Invalid password');
     }
+    // Return user data without password
+    const { password: _, ...userData } = user.toObject();
+    return userData;
 }
 
